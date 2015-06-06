56050b56e856b993c30786176b0dd8a76763341e
Basil Peace <grv87@yandex.ru>
2015-06-06 14:22:35 +0300

    Yoxos Launcher. Initial commit

---
+++ manual/yoxos-launcher/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'yoxos-launcher'
+$url = 'http://dzcriijehao7a.cloudfront.net/5.6.1/Yoxos_Installer-5.6.1-win32.win32.x86.exe'
+$url64 = 'http://dzcriijehao7a.cloudfront.net/5.6.1/Yoxos_Installer-5.6.1-win32.win32.x86_64.exe'
+
+Install-ChocolateyPackage $packageName 'EXE' '/S' $url $url64
