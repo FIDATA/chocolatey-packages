de0837c8a9c98f15d9c1e8e5d87e3cf2a64b28d9
Basil Peace <grv87@yandex.ru>
2018-03-13 14:18:11 +0300

    feat(sdelete): add SDelete 1.61.0.20160210

---
+++ manual/sdelete/1.61.0.20160210/tools/chocolateyInstall.ps1
@@ -0,0 +1,46 @@
+# Script to install SDelete 1.61 with Chocolatey
+# Copyright © 2018  Basil Peace
+# Based on work made by dtgm
+# at https://github.com/dtgm/chocolatey-packages/tree/master/automatic/_output/sdelete/1.61.0.20160210
+# 
+# This file is part of FIDATA Infrastructure.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$packageName = 'sdelete'
+$url = 'http://web.archive.org/web/20130508190727/http://download.sysinternals.com/files/SDelete.zip'
+$checksum = 'a7c5b5b25cfcc6d9609d7aec66e061a0938d4f9a'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+Write-Verbose "Accepting license..."
+$regRoot = 'HKCU:\Software\Sysinternals'
+$regPkg = 'SDelete'
+$regPath = Join-Path $regRoot $regPkg
+if (!(Test-Path $regRoot)) {New-Item -Path "$regRoot"}
+if (!(Test-Path $regPath)) {New-Item -Path "$regRoot" -Name "$regPkg"}
+Set-ItemProperty -Path "$regPath" -Name EulaAccepted -Value 1
+if ((Get-ItemProperty -Path "$regPath").EulaAccepted -ne 1) {
+  throw "Failed setting registry value."
+}
